51ef5b319b5c1a82439de9f1ba1a8d2b08dad9cc
Luís Ferreira <contact@lsferreira.net>
2019-11-21 10:45:14 +0000

    create tables for 'empregados' and 'gestores'
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/bank/create.sql
+++ src/bank/create.sql
@@ -52,19 +52,42 @@ CREATE TABLE operacao(
    CONSTRAINT pk_operacao PRIMARY KEY(id),
    CONSTRAINT fk_operacao_idCliente FOREIGN KEY(idCliente) REFERENCES cliente(id),
    CONSTRAINT fk_operacao_idConta FOREIGN KEY(idConta) REFERENCES conta(id)
-)
+);
 
 CREATE TABLE op_fecho(
    idOpF      NUMBER(7),
    razao      VARCHAR2(50) NOT NULL,
    CONSTRAINT pk_op_fecho PRIMARY KEY(idOpF),
    CONSTRAINT fk_op_fecho_idOpF FOREIGN KEY(idOpF) REFERENCES operacao(id)
-)
+);
 
 CREATE TABLE op_deplev(
    idOpDL     NUMBER(7),
    quantia    INTEGER NOT NULL,
    CONSTRAINT pk_op_deplev PRIMARY KEY(idOpDL),
    CONSTRAINT fk_op_deplev_idOpDL FOREIGN KEY(idOpDL) REFERENCES operacao(id)
-   CONSTRAINT ck_op_deplev_quantia CHECK(quantia >= -200 AND quantia < 2000)
+   CONSTRAINT ck_op_deplev_quantia CHECK(quantia >= -200 AND 5 < 2000)
+);
+
+CREATE TABLE op_abertura(
+   idOpA     NUMBER(7),
+   estado    VARCHAR2(2) NOT NULL,
+   CONSTRAINT pk_op_abertura PRIMARY KEY(idOpA),
+   CONSTRAINT fk_op_abertura_idOpA FOREIGN KEY(idOpA) REFERENCES operacao(id),
+   CONSTRAINT ck_op_abertura_estado CHECK(estado IN ('OK','MI','UV'))
+)
+
+CREATE TABLE empregado(
+   idEmp            NUMBER(7),
+   nome             VARCHAR2(50) NOT NULL,
+   data_contratacao DATE NOT NULL,
+   telefone         NUMBER(9),
+   CONSTRAINT pk_empregado PRIMARY KEY(id)
+)
+
+CREATE TABLE empregado_gestor(
+   idEmp           NUMBER(7),
+   email           VARCHAR2(50) NOT NULL,
+   CONSTRAINT pk_empregado_gestor PRIMARY KEY(idEmp),
+   CONSTRAINT fk_empregado_gestor FOREIGN KEY(idEmp) REFERENCES empregado(idEmp)
 )
